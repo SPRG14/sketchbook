fb427f5fd1b54aa7e67a8943fac236b834b7fa86
Sam Ogun <ogunayomipe@gmail.com>
2024-04-23 16:52:45 -0500

    Made ability to draw line and clear screen

--- src/project.py
+++ src/project.py
@@ -26,6 +26,48 @@ last_pos = None
 brush_size = DEFAULT_BRUSH_SIZE
 brush_color = DEFAULT_BRUSH_COLOR
 
+
+def draw_line(start, end, color, thickness):
+  pygame.draw.line(canvas, color, start, end, thickness)
+
+
+def handle_events():
+  global drawing, last_pos, brush_size, brush_color
+
+
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+      sys.exit()
+    elif event.type == pygame.MOUSEBUTTONDOWN:
+      drawing = True
+      last_pos = pygame.mouse.get_pos()
+    elif event.type == pygame.MOUSEBUTTONUP:
+      drawing = False
+    elif event.type == pygame.MOUSEMOTION:
+      if drawing:
+        current_pos = pygame.mouse.get_pos()
+        draw_line(last_pos, current_pos, brush_color, brush_size)
+        last_pos = current_pos
+    elif event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_c:
+        canvas.fill(WHITE)
+
+
+def main():
+  global brush_size, brush_color
+
+
+  while True:
+    handle_events()
+
+
+    screen.blit(canvas, (0, 0))
+
+
+    pygame.display.flip()
+
+
 if __name__ == "__main__":
   main()
 
